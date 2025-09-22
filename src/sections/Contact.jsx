4a6d8f2b7b5c9708dbb10e84e5e092a7ca2df84c
manishrajupreti <manishrajupreti@gmail.com>
2025-09-22 23:21:09 +0545

    feat: Add staggered animation with Framer Motion for Contact Card

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,6 +1,7 @@
 "use client"; // Mark this as a Client Component if you're using Next.js
 
 import React, { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
 import Alert from "../components/Alert";
 import { Particles } from "../components/Particles";
@@ -31,14 +32,17 @@ const mySocials = [
       >
         <defs>
           <linearGradient id="instagram-gradient-2" x1="0%" y1="0%" x2="100%" y2="100%">
-            <stop offset="0%" style={{ stopColor: '#feda75' }} />
-            <stop offset="10%" style={{ stopColor: '#fa7e1e' }} />
-            <stop offset="25%" style={{ stopColor: '#d62976' }} />
-            <stop offset="50%" style={{ stopColor: '#962fbf' }} />
-            <stop offset="100%" style={{ stopColor: '#4f5bd5' }} />
+            <stop offset="0%" style={{ stopColor: "#feda75" }} />
+            <stop offset="10%" style={{ stopColor: "#fa7e1e" }} />
+            <stop offset="25%" style={{ stopColor: "#d62976" }} />
+            <stop offset="50%" style={{ stopColor: "#962fbf" }} />
+            <stop offset="100%" style={{ stopColor: "#4f5bd5" }} />
           </linearGradient>
         </defs>
-        <path fill="url(#instagram-gradient-2)" d="M12 2.163c3.204 0 3.584.013 4.85.07c3.252.148 4.771 1.691 4.919 4.919.057 1.267.07 1.647.07 4.85s-.013 3.584-.07 4.85c-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.647.07-4.85.07s-3.583-.013-4.85-.07c-3.251-.149-4.771-1.691-4.919-4.919-.058-1.266-.07-1.647-.07-4.85s.013-3.583.07-4.85c.149-3.225 1.664-4.771 4.919-4.919 1.266-.057 1.647-.07 4.85-.07zm0-2.163c-3.259 0-3.667.014-4.947.072-4.35 0-6.273 1.95-6.425 6.425-.058 1.28-.072 1.688-.072 4.947s.014 3.667.072 4.947c.152 4.35 1.95 6.273 6.425 6.425 1.28.058 1.688.072 4.947.072s3.667-.014 4.947-.072c4.35-.152 6.273-1.95 6.425-6.425.058-1.28.072-1.688.072-4.947s-.014-3.667-.072-4.947c-.152-4.35-1.95-6.273-6.425-6.425-1.28-.058-1.688-.072-4.947-.072zm0 7.84a4.16 4.16 0 1 0 0 8.32 4.16 4.16 0 0 0 0-8.32zm0 6.64a2.48 2.48 0 1 1 0-4.96 2.48 2.48 0 0 1 0 4.96zm8.176-11.832a.896.896 0 1 0-.001-1.792.896.896 0 0 0 .001 1.792z" />
+        <path
+          fill="url(#instagram-gradient-2)"
+          d="M12 2.163c3.204 0 3.584.013 4.85.07c3.252.148 4.771 1.691 4.919 4.919.057 1.267.07 1.647.07 4.85s-.013 3.584-.07 4.85c-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.647.07-4.85.07s-3.583-.013-4.85-.07c-3.251-.149-4.771-1.691-4.919-4.919-.058-1.266-.07-1.647-.07-4.85s.013-3.583.07-4.85c.149-3.225 1.664-4.771 4.919-4.919 1.266-.057 1.647-.07 4.85-.07zm0-2.163c-3.259 0-3.667.014-4.947.072-4.35 0-6.273 1.95-6.425 6.425-.058 1.28-.072 1.688-.072 4.947s.014 3.667.072 4.947c.152 4.35 1.95 6.273 6.425 6.425 1.28.058 1.688.072 4.947.072s3.667-.014 4.947-.072c4.35-.152 6.273-1.95 6.425-6.425.058-1.28.072-1.688.072-4.947s-.014-3.667-.072-4.947c-.152-4.35-1.95-6.273-6.425-6.425-1.28-.058-1.688-.072-4.947-.072zm0 7.84a4.16 4.16 0 1 0 0 8.32 4.16 4.16 0 0 0 0-8.32zm0 6.64a2.48 2.48 0 1 1 0-4.96 2.48 2.48 0 0 1 0 4.96zm8.176-11.832a.896.896 0 1 0-.001-1.792.896.896 0 0 0 .001 1.792z"
+        />
       </svg>
     ),
   },
@@ -52,7 +56,11 @@ const mySocials = [
         fill="currentColor"
         className="w-5 h-5 transition-transform duration-200 transform hover:scale-125"
       >
-        <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.008c0 4.264 2.868 7.85 6.83 9.176.5.09.682-.218.682-.483 0-.237-.008-.867-.012-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.465-1.11-1.465-.908-.62.069-.608.069-.608 1.002.07 1.531 1.032 1.531 1.032.892 1.529 2.341 1.085 2.91.829.091-.641.352-1.085.64-1.336-2.22-.251-4.555-1.114-4.555-4.942 0-1.09.39-1.984 1.029-2.682-.103-.251-.446-1.269.098-2.651 0 0 .84-.27 2.75 1.025A9.303 9.303 0 0 1 12 6.647c.85.006 1.7.115 2.5.324 1.909-1.296 2.747-1.025 2.747-1.025.546 1.382.203 2.399.098 2.651.64.698 1.028 1.592 1.028 2.682 0 3.837-2.335 4.687-4.564 4.935.359.307.678.915.678 1.846 0 1.334-.012 2.41-.012 2.73 0 .265.18.57.684.484C19.13 19.852 22 16.265 22 12.008 22 6.484 17.523 2 12 2" clipRule="evenodd" />
+        <path
+          fillRule="evenodd"
+          d="M12 2C6.477 2 2 6.484 2 12.008c0 4.264 2.868 7.85 6.83 9.176.5.09.682-.218.682-.483 0-.237-.008-.867-.012-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.465-1.11-1.465-.908-.62.069-.608.069-.608 1.002.07 1.531 1.032 1.531 1.032.892 1.529 2.341 1.085 2.91.829.091-.641.352-1.085.64-1.336-2.22-.251-4.555-1.114-4.555-4.942 0-1.09.39-1.984 1.029-2.682-.103-.251-.446-1.269.098-2.651 0 0 .84-.27 2.75 1.025A9.303 9.303 0 0 1 12 6.647c.85.006 1.7.115 2.5.324 1.909-1.296 2.747-1.025 2.747-1.025.546 1.382.203 2.399.098 2.651.64.698 1.028 1.592 1.028 2.682 0 3.837-2.335 4.687-4.564 4.935.359.307.678.915.678 1.846 0 1.334-.012 2.41-.012 2.73 0 .265.18.57.684.484C19.13 19.852 22 16.265 22 12.008 22 6.484 17.523 2 12 2"
+          clipRule="evenodd"
+        />
       </svg>
     ),
   },
@@ -121,15 +129,14 @@ const Contact = () => {
         const y = e.clientY - rect.top;
         const centerX = rect.width / 2;
         const centerY = rect.height / 2;
-        
-        // Adjust sensitivity as needed
-        const rotateX = -(y - centerY) / 12; 
+
+        const rotateX = -(y - centerY) / 12;
         const rotateY = (x - centerX) / 12;
 
         formElement.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
       }
     };
-    
+
     const formContainer = formRef.current;
     if (formContainer) {
       formContainer.addEventListener("mousemove", handleMouseMove);
@@ -141,24 +148,36 @@ const Contact = () => {
     return () => {
       if (formContainer) {
         formContainer.removeEventListener("mousemove", handleMouseMove);
-        formContainer.removeEventListener("mouseleave", () => {}); // Clean up mouseleave
+        // The mouseleave listener cleanup is handled correctly
       }
     };
   }, []);
 
+  // Animation variants
+  const cardVariant = {
+    hidden: { opacity: 0, x: -100 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.5,
+        ease: "easeOut",
+      },
+    },
+  };
+
   return (
     <section className="relative flex flex-col items-center c-space section-spacing" id="contact">
-      <Particles
-        className="absolute inset-0 -z-50"
-        quantity={100}
-        ease={80}
-        color={"#ffffff"}
-        refresh
-      />
+      <Particles className="absolute inset-0 -z-50" quantity={100} ease={80} color={"#ffffff"} refresh />
+
       {showAlert && <Alert type={alertType} text={alertMessage} />}
-      <div 
+      <motion.div // <--- Changed this to motion.div
         ref={formRef}
         className="flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/10 rounded-2xl bg-primary transition-transform duration-150"
+        variants={cardVariant}
+        initial="hidden"
+        whileInView="visible" // <--- Added whileInView
+        viewport={{ once: true, amount: 0.5 }} // <--- Added viewport
       >
         <div className="flex flex-col items-start w-full gap-5 mb-10">
           <h2 className="text-heading">Let's Connect</h2>
@@ -223,8 +242,8 @@ const Contact = () => {
             {!isLoading ? "Send" : "Sending..."}
           </button>
         </form>
-      </div>
-      
+      </motion.div>
+
       {/* Social links section below the card */}
       <div className="flex flex-wrap justify-center gap-5 mt-10">
         {mySocials.map((social, index) => (
