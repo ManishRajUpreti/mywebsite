66d30e57fb9faacc927b1cadb36efa1a0e2f1903
manishrajupreti <manishrajupreti@gmail.com>
2025-09-27 21:03:58 +0545

    feat: Add staggered animation with Framer Motion

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -3,6 +3,19 @@ import { myProjects } from "../constants";
 import { motion } from "framer-motion";
 
 const Projects = () => {
+  const cardVariant = {
+    hidden: { opacity: 0, x: -300 },
+    visible: (i) => ({
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: i * 0.1, // stagger by index
+        duration: 0.1,  // slow motion
+        ease: "easeOut",
+      },
+    }),
+  };
+
   return (
     <motion.section
       initial={{ opacity: 0, y: 50 }}
@@ -15,11 +28,18 @@ const Projects = () => {
       <h2 className="text-heading">My Selected Projects</h2>
       <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full" />
 
-      <div className="grid grid-cols-1 gap-8 mt-10 md:grid-cols-2 lg:grid-cols-2">
+      <motion.div 
+        className="grid grid-cols-1 gap-8 mt-10 md:grid-cols-2 lg:grid-cols-2"
+        variants={cardVariant}
+        initial="hidden"
+        whileInView="visible"
+        custom={0}
+        viewport={{ once: true, amount: 0.5 }}
+      >
         {myProjects.map((project) => (
           <Project key={project.id} {...project} />
         ))}
-      </div>
+      </motion.div>
     </motion.section>
   );
 };
