24664c97996983d1a0e64e117e7d061df54c8a25
manishrajupreti <manishrajupreti@gmail.com>
2025-11-20 22:07:13 +0545

    chore: Update the styles across the site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import { max } from 'three/tsl';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '8xl': '125rem',
+      },
+    },
   },
   plugins: [],
 };
